339649694f1768d6f3b18c534c7d8d46b46d43bb
mic3ff <37799645+mic3ff@users.noreply.github.com>
2022-10-18 21:03:04 +0200

    todo_wsjf_experiments.py
    2022-10-18 added urgency/importance > strategy calculations

--- todo_wsjf_experiments.py
+++ todo_wsjf_experiments.py
@@ -8,39 +8,92 @@ Cost of delay = User-business Value + Time criticality + Risk reduction and/or O
     User-business Value = (1, 2, 3, 5, 8, 13, 20)
     Time criticality = (1, 2, 3, 5, 8, 13, 20)
     Risk reduction and/or Opportunity enablement = (1, 2, 3, 5, 8, 13, 20)
+
 Wynik:
 - Spriorytetyzowana lista zadan
 - dodawanie informacji o statusie, mozliwosc zmiany statusu
 - wyswietlanie w kolumnach (kanban):
+
 -
 '''
 
 import os
 import time
 from tabulate import tabulate
+import re
+from sys import platform
 
-#widnows:
-cmd = "cls"
-#linux:
 #cmd = "clear"
-
 file_location = "."
 tasks = list()
 
-def createTask():
+def operatingSystemCheck():
+    global cmd
+    if platform == "win32":
+        cmd = "cls"
+        print("Running Windows")
+    elif "linux" or platform == "linux2":
+        cmd = "clear"
 
-    '''
-    def checkCorrcectValue(value):
-        fibonnaci_values = [1, 2, 3, 5, 8, 13, 20]
-        if value not in fibonnaci_values:
-            print("please provide proper value: 1, 2, 3, 5, 8, 13, 20")
-            #
-    '''
+    return(cmd)
+
+
+def saveTaskToFile():
+    f = open("todo_wsfj", "a")
+    #f.write(str(task))
+    f.write(str(task)+"\n")
+    f.close()
+
+
+def readFromFile():
+
+    global tasks
+
+    tasks = [line.split(',') for line in open("todo_wsfj")]
+
+    return(tasks)
+
+
+def checkCorrcectValue(variable):
+    global anwser
+    anwser = ''
+    while(True):
+
+        try:
+            anwser = int(input(str(variable+": ")))
+        except:
+            print('Wrong input. Please enter a number ...')
+        #Check what choice was entered and act accordingly
+        if anwser not in  [1, 2, 3, 5, 8, 13, 20]:
+            print("provide number 1, 2, 3, 5, 8, 13 or 20")
+        else:
+            variable = anwser
+            break
+
+    return(variable)
+
+def calculateStrategy(u, i):
+    global strategy
+    if u == "n" and i == "n":
+        strategy = "Forget it"
+    elif u == "n" and i == "y":
+        strategy = "Schedule it"
+    elif u == "y" and i =="n":
+        strategy = "Delegate"
+    elif u == "y" and i =="y":
+        strategy = "Do it, now!"
+    return(strategy)
+
+
+def createTask():
     '''
     DONE: add task (summary, description, Cosy of delay, Job size, User-business Value, Time criticality, Risk reduction and/or Opportunity enablement)
     result: input data saved as list
     '''
+    fibonnaci_values = [1, 2, 3, 5, 8, 13, 20]
+
     global summary
+    #checkCorrcectValue(summary) #IN PROGRESS
     summary = input("summary: ")
     global value
     value = int(input("value: "))
@@ -50,8 +103,12 @@ def createTask():
     timeCriticality = int(input("time criticality: "))
     global riskReduction
     riskReduction = int(input("risk reduction: "))
+    global urgency
+    urgency = (input("urgent? (y/n): "))
+    global importance
+    importance = (input("important? (y/n): "))
     print()
-    print("...task created")
+
 
     global task
     task = list()
@@ -60,10 +117,30 @@ def createTask():
 
     calulateWSJF(cost_of_delay, size)
 
-    task.extend([summary, value, size, timeCriticality, riskReduction, cost_of_delay, wsjf])
+    calculateStrategy(urgency, importance)
 
-    saveToFile(file_location)
-    #tasks.append(task)
+    task.extend([summary, value, size, timeCriticality, riskReduction, cost_of_delay, wsjf, urgency, importance, strategy])
+
+    #convert list to string
+    task = str(task)
+
+    #test: task as string
+    #print(task)
+
+    # remove [, ], ',
+    task = re.sub('[\[\]\']', '', task)
+
+    #test: if [, ], ' removed
+    #print(task)
+
+    print("...task created")
+    time.sleep(0.2)
+
+    # save task to file
+    saveTaskToFile()
+    print("...task saved")
+
+    #tasks.append(task) zmiana: zapisujemy pojedyncze taski do pliku
 
 def editTask(task):
     '''
@@ -105,26 +182,16 @@ def saveToFile(file_location):
     DONE: save list "tasks" into file so that I can close the app and get back in future
     '''
     f = open("todo_wsfj", "a")
-    f.write(str(task)+"\n")
+    for task in tasks:
+        f.write(str(task)+"\n")
 
     f.close()
 
-def readFromFile():
-    global tasks
-    #file = str(file_location)+"/todo_wsfj"
-    #file = "todo_wsfj"
-    #print("opening file: "+ str(file))
+def sortTasks(list): # sort by WSJF
+    for task in tasks:
+        task[6] = float(task[6])
+        task[6] = round(task[6], 3)
 
-    #tasks = open("todo_wsfj").readlines()
-    tasks = [line.split(',') for line in open("todo_wsfj")]
-
-    #with open('file') as f:
-    #    tasks = [line for line in f]
-    #print(tasks)
-    #print("ziemniak")
-    return(tasks)
-
-def sortTasks(list):
     tasks.sort(key=lambda x:x[6])
 
 
@@ -133,7 +200,7 @@ menu_options = {
     2: 'add task',
     3: 'edit task (in progress)',
     4: 'delete task (not started)',
-    5: 'save and exit'
+    5: 'exit'
 }
 
 def print_menu():
@@ -147,16 +214,33 @@ def option1():
     print("Display tasks:")
     print()
 
-    # TODO: change to read task from file
-
     readFromFile()
-    #print("tutaj")
 
+    sortTasks(tasks)
+
+    tasks.reverse()
+
+    #tasks
+    #for task in tasks:
+    #    task[0] = task[0][:20]
 
     ## Tables, take 1
-    sortTasks(tasks)
-    print (tabulate(tasks, headers=["Task", "Value", "Size", "Crit.", "Risk Red.", "CoD", "WSJF"]))
+    print(tabulate(tasks, headers=["TASK", "VALUE", "SIZE", "CRIT.", "RISK RED.", "CoD", "WSJF", "IMP.", "URG.", "STRATEGY"], tablefmt='grid'))
 
+    # display onlyu summary, wsjf and strategy:
+    '''
+    new_tasks = list()
+    for task in tasks:
+        new_task = list()
+        #new_task = (task[0], task[6], task[9])
+        new_task.append(task[0])
+        new_task.append(task[6])
+        new_task.append(task[7])
+
+        new_tasks.append(new_task)
+
+    print(tabulate(new_tasks, headers=["TASK", "WSJF", "TEST"], tablefmt='grid'))
+    '''
 
 
     '''
@@ -173,7 +257,6 @@ def option2():
     print()
 
     createTask()
-    #saveToFile(file_location)
 
 def option3():
     os.system(cmd)
@@ -200,7 +283,7 @@ def option4():
     print("Handle option Option 4")
 
 if __name__=='__main__':
-
+    operatingSystemCheck()
     os.system(cmd)
 
     while(True):
@@ -227,10 +310,12 @@ if __name__=='__main__':
             option4()
             print()
         elif option == 5:
+            '''
             print('...saving...')
-            #saveToFile(file_location)
+            saveToFile(file_location)
             time.sleep(0.5)
             print("...work saved...")
+            '''
             time.sleep(0.5)
             print("...closing")
             time.sleep(0.5)
